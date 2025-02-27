6d9f69d1727f9b067706877398f9b2f8ebbcb1fc
Alex <asolomon413@gmail.com>
2025-02-27 07:49:50 -0800

    Character Movement Enabled
    Player can now move around the game
    -Added script to VRPlayerObject for movement
    -Player toggle's walking when screen tapped
    -Added Character Controller component to VR Player

--- Assets/HeadTracking.cs
+++ Assets/HeadTracking.cs
@@ -14,10 +14,12 @@ public class HeadTracking : MonoBehaviour
 
     void Update()
     {
-        if (SystemInfo.supportsGyroscope)
+        if (SystemInfo.supportsGyroscope) //Make sure Gyro is possible
         {
-            Quaternion deviceRotation = Input.gyro.attitude;
-            transform.rotation = Quaternion.Euler(90f, 0f, 0f) * new Quaternion(deviceRotation.x, deviceRotation.y, -deviceRotation.z, -deviceRotation.w);
+            Quaternion deviceRotation = Input.gyro.attitude; //Get the device gyro
+            
+            //Move game camera with device gyro
+            transform.rotation = Quaternion.Euler(90f, 0f, 0f) * new Quaternion(deviceRotation.x, deviceRotation.y, -deviceRotation.z, -deviceRotation.w); //Move game camera with device gyro
         }
     }
 }
